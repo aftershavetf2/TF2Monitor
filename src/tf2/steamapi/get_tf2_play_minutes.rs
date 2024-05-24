f12a291aae6207f6d9bbf6f50dafc0d3d66b2b18
aftershavetf2 <aftershavetf2@gmail.com>
2024-05-24 18:30:33 +0200

    Update get_tf2_play_minutes.rs

--- src/tf2/steamapi/get_tf2_play_minutes.rs
+++ src/tf2/steamapi/get_tf2_play_minutes.rs
@@ -2,25 +2,25 @@ use crate::models::steamid::SteamID;
 use reqwest::{blocking::get, Result};
 use serde::Deserialize;
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct Game {
     appid: u32,
     playtime_forever: u32,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct Payload {
     games: Vec<Game>,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct Envelope {
     response: Payload,
 }
 
 pub fn get_tf2_play_minutes(steam_api_key: &String, steamid: SteamID) -> Option<u32> {
     let url = format!(
-        "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key={}&steamid={}&format=json",
+        "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key={}&steamid={}&count=50&format=json",
         steam_api_key, steamid.to_u64()
     );
 
@@ -29,12 +29,19 @@ pub fn get_tf2_play_minutes(steam_api_key: &String, steamid: SteamID) -> Option<
         Ok(response) => {
             let reply: Result<Envelope> = response.json();
             match reply {
-                Ok(reply) => reply
-                    .response
-                    .games
-                    .iter()
-                    .find(|g| g.appid == 440)
-                    .map(|game| game.playtime_forever),
+                Ok(reply) => {
+                    // log::info!("Reply: {:?}", reply);
+                    match reply
+                        .response
+                        .games
+                        .iter()
+                        .find(|g| g.appid == 440)
+                        .map(|game| game.playtime_forever)
+                    {
+                        Some(playtime) => Some(playtime),
+                        None => Some(0),
+                    }
+                }
                 // The reply was not in the expected format, probably just "{}" because of an private profile
                 Err(_) => Some(0),
             }
