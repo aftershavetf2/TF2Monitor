fa53da661b8357aa075038093103210393842d04
aftershavetf2 <aftershavetf2@gmail.com>
2024-05-25 18:34:26 +0200

    Update scoreboard_team.rs

--- src/gui/scoreboard_team.rs
+++ src/gui/scoreboard_team.rs
@@ -170,15 +170,15 @@ fn add_team_symbol(app_win: &mut AppWin, ui: &mut Ui, self_steamid: SteamID, pla
                 response.on_hover_text("This is you");
             }
 
-            if let Some(tooltip) = &player.has_steam_bans() {
-                let (rect, response) = ui.allocate_at_least(size, Sense::hover());
-                ui.painter().rect_filled(rect, 3.0f32, Color32::RED);
-                response.on_hover_text(tooltip);
-            }
-
             if let Some(tooltip) = &player.is_newbie() {
                 let (rect, response) = ui.allocate_at_least(size, Sense::hover());
                 ui.painter().rect_filled(rect, 3.0f32, Color32::GREEN);
+                response.on_hover_text(tooltip);
+            }
+
+            if let Some(tooltip) = &player.has_steam_bans() {
+                let (rect, response) = ui.allocate_at_least(size, Sense::hover());
+                ui.painter().rect_filled(rect, 3.0f32, Color32::RED);
                 response.on_hover_text(tooltip);
             }
 
