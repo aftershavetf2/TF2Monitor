7cdecba056d0aaac06fa8220c863617bc962cff9
aftershavetf2 <aftershavetf2@gmail.com>
2024-05-26 21:53:08 +0200

    Update recently_left.rs

--- src/gui/recently_left.rs
+++ src/gui/recently_left.rs
@@ -1,6 +1,8 @@
 use crate::models::AppWin;
 use eframe::egui::Ui;
 
+use super::player_tooltip::add_player_tooltip;
+
 pub fn add_recently_left_players(app_win: &mut AppWin, ui: &mut Ui) {
     if app_win.lobby.recently_left_players.is_empty() {
         return;
@@ -25,7 +27,8 @@ pub fn add_recently_left_players(app_win: &mut AppWin, ui: &mut Ui) {
             }
 
             ui.hyperlink_to(player.name.as_str(), player.steamid.steam_history_url())
-                .on_hover_text("View on Steam History");
+                .on_hover_text("Click to view on Steam History")
+                .on_hover_ui_at_pointer(|ui| add_player_tooltip(app_win, ui, player));
         }
     });
 }
