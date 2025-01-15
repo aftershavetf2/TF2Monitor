d6d2c72242ef7658c242a5792be76aa0a84b9977
aftershavetf2 <aftershavetf2@gmail.com>
2025-01-15 10:08:55 +0100

    Update chat.rs

--- src/gui/chat.rs
+++ src/gui/chat.rs
@@ -109,26 +109,34 @@ fn add_chat_row(ui: &mut Ui, app_win: &mut AppWin, chat_row: &LobbyChat) {
             },
         );
 
+        job.append(
+            &chat_row.message,
+            0.0,
+            TextFormat {
+                color: Color32::from_rgb(210, 210, 210),
+                ..Default::default()
+            },
+        );
+
         let is_translated = chat_row.translated_message.is_some()
             && chat_row.translated_message != Some(chat_row.message.clone());
 
         // The chat message
         if is_translated {
             job.append(
-                &chat_row.translated_message.as_ref().unwrap(),
+                " ",
+                0.0,
+                TextFormat {
+                    color: Color32::from_rgb(210, 210, 210),
+                    ..Default::default()
+                },
+            );
+            job.append(
+                format!("({})", &chat_row.translated_message.as_ref().unwrap()).as_str(),
                 0.0,
                 TextFormat {
                     color: Color32::from_rgb(255, 255, 255),
                     background: Color32::from_rgb(20, 20, 20),
-                    ..Default::default()
-                },
-            );
-        } else {
-            job.append(
-                &chat_row.message,
-                0.0,
-                TextFormat {
-                    color: Color32::from_rgb(210, 210, 210),
                     ..Default::default()
                 },
             );
